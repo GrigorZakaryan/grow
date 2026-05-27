91089482fd36cd1902c0f3e9e5a9aaa6fa2a2fa2
Grig <grogorzaqaryan@gmail.com>
2026-05-27 19:46:25 +0200

    Changed the icon and removed the redirect

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import { redirect } from "next/navigation";
 
 export default function Home() {
-  redirect("/home");
   return (
     <div className="relative w-full h-dvh">
       <Link className="text-white" href={"/home"}>
